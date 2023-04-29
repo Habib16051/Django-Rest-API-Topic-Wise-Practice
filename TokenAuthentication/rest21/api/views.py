cd3c80232ceb6249b7285a3883b0ba3562c4e7dc
habib <habibmhr143@gmail.com>
2023-04-29 18:28:48 +0600

    Token Auth Httpie and many more

---
+++ TokenAuthentication/rest21/api/views.py
@@ -0,0 +1,24 @@
+from .models import Student
+from .serializers import StudentSerializers
+from rest_framework import viewsets
+from rest_framework.authentication import BasicAuthentication, SessionAuthentication, TokenAuthentication
+from rest_framework.permissions import IsAuthenticated, AllowAny, IsAdminUser, IsAuthenticatedOrReadOnly, \
+    DjangoModelPermissions, DjangoModelPermissionsOrAnonReadOnly
+
+
+# Create, Retrieve, Update and Destroy all will be work at this time
+class StudentModelViewSet(viewsets.ModelViewSet):
+    queryset = Student.objects.all()
+    serializer_class = StudentSerializers
+    authentication_classes = [TokenAuthentication]
+    # permission_classes = [IsAuthenticated]
+    # permission_classes = [AllowAny]
+    # permission_classes = [IsAdminUser]
+    permission_classes = [IsAuthenticatedOrReadOnly]
+    # permission_classes = [DjangoModelPermissions]
+    # permission_classes = [DjangoModelPermissionsOrAnonReadOnly]
+
+# # Read Only ViewSet - only List and retrieve operation will be work at this time
+# class ReadOnlyStudentModelViewSet(viewsets.ReadOnlyModelViewSet):
+#     queryset = Student.objects.all()
+#     serializer_class = StudentSerializers
