2ed72da60bf5220c382aafbbaff727c2a16020fb
habib <habibmhr143@gmail.com>
2023-04-29 14:03:05 +0600

    yes file has been added

---
+++ CustomPermission/rest18/api/customspermissions.py
@@ -0,0 +1,11 @@
+from rest_framework.permissions import BasePermission
+
+
+class MyPermission(BasePermission):
+    def has_permission(self, request, view):
+        if request.method == 'POST':
+            return True
+        return False
+
+
+
--- CustomPermission/rest18/api/views.py
+++ CustomPermission/rest18/api/views.py
@@ -1,6 +1,7 @@
 from .models import Student
 from .serializers import StudentSerializers
 from rest_framework import viewsets
+from .customspermissions import MyPermission
 from rest_framework.authentication import BasicAuthentication, SessionAuthentication
 from rest_framework.permissions import IsAuthenticated, AllowAny, IsAdminUser, IsAuthenticatedOrReadOnly, \
     DjangoModelPermissions, DjangoModelPermissionsOrAnonReadOnly
@@ -11,12 +12,12 @@ class StudentModelViewSet(viewsets.ModelViewSet):
     queryset = Student.objects.all()
     serializer_class = StudentSerializers
     authentication_classes = [SessionAuthentication]
-    # permission_classes = [IsAuthenticated]
+    permission_classes = [MyPermission]
     # permission_classes = [AllowAny]
     # permission_classes = [IsAdminUser]
     # permission_classes = [IsAuthenticatedOrReadOnly]
     # permission_classes = [DjangoModelPermissions]
-    permission_classes = [DjangoModelPermissionsOrAnonReadOnly]
+    # permission_classes = [DjangoModelPermissionsOrAnonReadOnly]
 
 # # Read Only ViewSet - only List and retrieve operation will be work at this time
 # class ReadOnlyStudentModelViewSet(viewsets.ReadOnlyModelViewSet):
