f0f85f00ab68df1433c7013df7ae5e996144ce4d
habib <habibmhr143@gmail.com>
2023-05-01 21:37:21 +0600

    Nested Serializer in DRF

---
+++ Nested Serializer in DRF/rest34/api/serializers.py
@@ -0,0 +1,21 @@
+from rest_framework import serializers
+from .models import Singer, Song
+
+
+class SongSerializers(serializers.ModelSerializer):
+    class Meta:
+        model = Song
+        fields = '__all__'
+
+
+class SingerSerializers(serializers.ModelSerializer):
+    # song = serializers.StringRelatedField(many=True, read_only=True)
+    # song = serializers.PrimaryKeyRelatedField(many=True, read_only=True)
+    # song = serializers.HyperlinkedRelatedField(many=True, read_only=True, view_name='song-detail')
+    # song = serializers.SlugRelatedField(many=True, read_only=True, slug_field='title')
+    song = serializers.HyperlinkedIdentityField(view_name='song-detail')
+    # sungby = SongSerializers(many=True, read_only=True)
+
+    class Meta:
+        model = Singer
+        fields = '__all__'
---
+++ Nested Serializer in DRF/rest34/rest34/urls.py
@@ -0,0 +1,22 @@
+
+from django.contrib import admin
+from django.urls import path,include
+from rest_framework.routers import DefaultRouter
+from api import views
+
+
+# created Default object router
+
+router = DefaultRouter()
+
+# Register StudentViewSet With Router
+router.register('singer', views.SingerViewSet, basename='singer'),
+router.register('song', views.SongViewSet, basename='song'),
+
+
+urlpatterns = [
+    path('admin/', admin.site.urls),
+    path('', include(router.urls)),
+    path('auth/', include('rest_framework.urls', namespace='rest_framework')),
+
+]
